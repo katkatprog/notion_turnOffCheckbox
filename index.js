1f2461085255d89757f8305da2086338c3ab2a04
katkatprog <katkatprog@gmail.com>
2022-05-07 01:49:35 +0900

    定期実行できるよう，node-cronモジュールを導入

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { Client } from "@notionhq/client"
 import { } from 'dotenv/config'
+import cron from 'node-cron'
 
+// ---------------関数定義部分--------------
 const notion = new Client({ auth: process.env.NOTION_KEY })
 
 async function getDatabase() {
@@ -24,18 +26,23 @@ async function checkboxTurnedToOff(pageID) {
     console.error(error.body)
   }
 }
+// ---------------関数定義部分終了--------------
 
-const notionDBObj = await getDatabase();//DB取得
-const notionDBElements = notionDBObj.results//取得したDBの要素部分
+// ---------------処理実行部分----------------
+// 毎日00:00:00秒に実行する
+cron.schedule('0 0 0 * * *', async () => {
+  const notionDBObj = await getDatabase();//DB取得
+  const notionDBElements = notionDBObj.results//取得したDBの要素部分
 
-let pageIds = []//DBの各要素のpageIdを格納 ↓3行で格納処理を行う。
-notionDBElements.forEach(ele => {
-  pageIds.push(ele.id)
-});
+  let pageIds = []//DBの各要素のpageIdを格納 ↓3行で格納処理を行う。
+  notionDBElements.forEach(ele => {
+    pageIds.push(ele.id)
+  });
 
-// 各々のpageId(DBの要素)のうち，チェックボックスがtrue(チェックあり)のもののみチェックを外す。
-for (let i = 0; i < pageIds.length; i++) {
-  if (notionDBElements[i].properties.DONE.checkbox) {
-    checkboxTurnedToOff(pageIds[i])
+  // 各々のpageId(DBの要素)のうち，チェックボックスがtrue(チェックあり)のもののみチェックを外す。
+  for (let i = 0; i < pageIds.length; i++) {
+    if (notionDBElements[i].properties.DONE.checkbox) {
+      checkboxTurnedToOff(pageIds[i])
+    }
   }
-}
+})
